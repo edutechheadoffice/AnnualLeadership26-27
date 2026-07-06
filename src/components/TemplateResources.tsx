6b140534775c7abc9664039a7a9dd1814279c7aa
edutechheadoffice <edutechsdhslh@gmail.com>
2026-07-06 09:48:30 +0700

    tampah link teams

--- src/components/TemplateResources.tsx
+++ src/components/TemplateResources.tsx
@@ -127,7 +127,7 @@ export default function TemplateResources() {
 
           <div className="shrink-0">
             <a
-              href="https://teams.microsoft.com/meet/48927116191784?p=N3gOm9iAnh87UBgZs5"
+              href="https://teams.microsoft.com/meet/4820714941933?p=ACsGXVgGvTqCTdP7oX"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex items-center justify-center gap-3 px-8 py-4 bg-brand-orange text-white font-bold uppercase tracking-wider rounded-full hover:bg-brand-orange/90 hover:shadow-lg hover:shadow-brand-orange/20 transition-all duration-300 group"
